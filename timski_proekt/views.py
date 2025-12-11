2d4d6fb07ee588742f425470ad35b985aac73a04
Damjan <serafimovskid77@gmail.com>
2025-12-11 23:31:08 +0100

    edit

--- timski_proekt/views.py
+++ timski_proekt/views.py
@@ -5,8 +5,9 @@ import json
 from django.shortcuts import render
 
 def index(request):
-    with open("timski_proekt/Prasalnici/20meseci.json", encoding="utf-8") as f:
+    with open("timski_proekt/Prasalnici/2meseci.json", encoding="utf-8") as f:
+
+
 
         data = json.load(f)
-
     return render(request, "index.html", {"quiz": data})
